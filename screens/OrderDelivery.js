c95c85322bd506a915eac86989c6caeadf0546cd
Abel Masila <abelmasila@gmail.com>
2021-01-29 18:47:22 +0300

    added screens folder with all required screens

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const OrderDelivery = () => {
+  return (
+    <View>
+      <Text>Order Delivery</Text>
+    </View>
+  );
+};
+export default OrderDelivery;
