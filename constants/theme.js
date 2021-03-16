6be34633afb0e8d1dc80c72213c35fc59d04f37f
Abel Masila <abelmasila@gmail.com>
2021-03-17 01:07:24 +0300

    added main categories component

--- constants/theme.js
+++ constants/theme.js
@@ -53,11 +53,11 @@ export const FONTS = {
   h2: {fontFamily: 'System', fontSize: SIZES.h2, lineHeight: 30},
   h3: {fontFamily: 'System', fontSize: SIZES.h3, lineHeight: 22},
   h4: {fontFamily: 'System', fontSize: SIZES.h4, lineHeight: 22},
-  body1: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body1, lineHeight: 36},
-  body2: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body2, lineHeight: 30},
-  body3: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body3, lineHeight: 22},
-  body4: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body4, lineHeight: 22},
-  body5: {fontFamily: 'Roboto-Regular', fontSize: SIZES.body5, lineHeight: 22},
+  body1: {fontFamily: 'System', fontSize: SIZES.body1, lineHeight: 36},
+  body2: {fontFamily: 'System', fontSize: SIZES.body2, lineHeight: 30},
+  body3: {fontFamily: 'System', fontSize: SIZES.body3, lineHeight: 22},
+  body4: {fontFamily: 'System', fontSize: SIZES.body4, lineHeight: 22},
+  body5: {fontFamily: 'System', fontSize: SIZES.body5, lineHeight: 22},
 };
 
 const appTheme = {COLORS, SIZES, FONTS};
--- screens/Home.js
+++ screens/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   View,
@@ -406,7 +407,80 @@ const Home = () => {
       </View>
     );
   }
-  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
+
+  function renderMainCategories() {
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{
+            padding: SIZES.padding,
+            paddingBottom: SIZES.padding * 2,
+            backgroundColor:
+              selectedCategory?.id == item.id ? COLORS.primary : COLORS.white,
+            borderRadius: SIZES.radius,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: SIZES.padding,
+            ...styles.shadow,
+          }}
+          onPress={() => onSelectCategory(item)}>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor:
+                selectedCategory?.id == item.id
+                  ? COLORS.white
+                  : COLORS.lightGray,
+            }}>
+            <Image
+              source={item.icon}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          </View>
+
+          <Text
+            style={{
+              marginTop: SIZES.padding,
+              color:
+                selectedCategory?.id == item.id ? COLORS.white : COLORS.black,
+              ...FONTS.body5,
+            }}>
+            {item.name}
+          </Text>
+        </TouchableOpacity>
+      );
+    };
+    return (
+      <View style={{padding: SIZES.padding * 2}}>
+        <Text style={{...FONTS.h1}}>Main</Text>
+        <Text style={{...FONTS.h1}}>Categories</Text>
+        <FlatList
+          data={categories}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => `${item.id}`}
+          renderItem={renderItem}
+          contentContainerStyle={{
+            paddingVertical: SIZES.padding * 2,
+          }}
+        />
+      </View>
+    );
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+      {renderMainCategories()}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
